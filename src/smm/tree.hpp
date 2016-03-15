bad1f35c31deff0046979f0aa44a97c69517d39d
Dmitry Vlasov <vlasov@academ.org>
2016-03-15 12:14:12 +0800

    a little comment removed

--- src/smm/tree.hpp
+++ src/smm/tree.hpp
@@ -8,8 +8,6 @@ namespace mdl { namespace smm {
 typedef map<uint, uint> Perm;
 typedef map<uint, Perm> Transform;
 
-//ostream& operator << (ostream& os, const tree::Perm&);
-
 Proof* to_tree(const Proof* pr);
 Proof* to_rpn(const Proof* pr);
 void transform(Proof* proof, const Transform& trans, bool forward = true);
