7b83dedb03229a087c1d4a8bc6d25596e9c94201
d_vlasov <dvlasov@adjuggler.ru>
2016-05-11 20:08:44 +0600

    trabslation script enhanced

--- transl.sh
+++ transl.sh
@@ -10,7 +10,20 @@ fi
 cp ${file}.mm ./tmp/${file}.mm
 cd tmp
 
+echo "mm -> smm"
 ../bin/release/mm  -v -i ${file}.mm  -o $file.smm
+echo -e 'done\n\n'
+
+echo "smm -> rus"
 ../bin/release/smm -v -i ${file}.smm -o ${file}.rus
+echo -e 'done\n\n'
+
+echo "rus -> smm"
 ../bin/release/mdl -v -i ${file}.rus -o ${file}-1.smm
+echo -e 'done\n\n'
+
+echo "verify smm"
 ../bin/release/smm -v -i ${file}-1.smm
+echo -e 'done\n'
+echo -e 'translation tests passed =)\n'
+
