ad316fb299ea0bc20b03a933ee6e686a93e2d98d
Dmitry Vlasov <vlasov@academ.org>
2016-06-05 17:57:31 +0600

    translation script is fixed

--- transl.sh
+++ transl.sh
@@ -22,12 +22,21 @@ echo "rus -> rus'"
 ../bin/release/mdl -v -g -i ${file}.rus -o ${file}-1.rus
 echo -e 'done\n\n'
 
-echo "rus -> smm"
+echo "rus' -> smm'"
 ../bin/release/mdl -v -i ${file}-1.rus -o ${file}-1.smm
 echo -e 'done\n\n'
 
-echo "verify smm"
+echo "verify smm'"
 ../bin/release/smm -v -i ${file}-1.smm
 echo -e 'done\n'
+
+echo "smm' -> mm'"
+../bin/release/smm -v -i ${file}-1.smm -o ${file}-1.mm
+echo -e 'done\n\n'
+
+echo "verify mm'"
+../bin/release/mm -v -i ${file}-1.mm
+echo -e 'done\n'
+
 echo -e 'translation tests passed =)\n'
 
