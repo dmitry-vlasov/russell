45ef4835f8d8b9507d344bef714c548f8d79ce0a
d_vlasov <dvlasov@adjuggler.ru>
2016-07-12 18:40:07 +0600

    redundant comments are removed

--- include/mm/globals.hpp
+++ include/mm/globals.hpp
@@ -1,15 +1,3 @@
-/*****************************************************************************/
-/* Project name:    smm - verifier for the Simplified MetaMath language      */
-/* File name:       smm_auxiliary_Volume.hpp                                 */
-/* Description:     volume counters                                          */
-/* Copyright:       (c) 2006-2010 Dmitri Vlasov                              */
-/* Author:          Dmitri Yurievich Vlasov, Novosibirsk, Russia             */
-/* Email:           vlasov at academ.org                                     */
-/* URL:             http://mathdevlanguage.sourceforge.net                   */
-/* Modified by:                                                              */
-/* License:         GNU General Public License Version 3                     */
-/*****************************************************************************/
-
 #pragma once
 
 #include "mm/ast.hpp"
--- include/smm/ast.hpp
+++ include/smm/ast.hpp
@@ -1,15 +1,3 @@
-/*****************************************************************************/
-/* Project name:    smm - verifier for the Simplified MetaMath language      */
-/* File Name:       smm_ast_Source.hpp                                       */
-/* Description:     smm source                                               */
-/* Copyright:       (c) 2006-2010 Dmitri Vlasov                              */
-/* Author:          Dmitri Yurievich Vlasov, Novosibirsk, Russia             */
-/* Email:           vlasov at academ.org                                     */
-/* URL:             http://mathdevlanguage.sourceforge.net                   */
-/* Modified by:                                                              */
-/* License:         GNU General Public License Version 3                     */
-/*****************************************************************************/
-
 #pragma once
 
 #include "common.hpp"
