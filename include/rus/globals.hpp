cdd2084dc1fcac7a8e113cecca13f112034d6c2e
Dmitry Vlasov <vlasov@academ.org>
2016-12-12 10:40:18 +0700

    logs directory is added to mdld config

--- include/rus/globals.hpp
+++ include/rus/globals.hpp
@@ -93,13 +93,19 @@ smm::Source* translate(const Source* source);
 
 namespace daemon {
 
+#define DEFAULT_DAEMON_LOGS "/var/tmp/mdld.log"
+
 struct Config {
 	enum {
 		DEFAULT_PORT = 808011
 	};
-	Config() : port(DEFAULT_PORT) { }
+	static const Config& get() { return mod(); }
 	static Config& mod() { static Config d; return d; }
 	uint   port;
+	string logs;
+
+private:
+	Config() : port(DEFAULT_PORT), logs(DEFAULT_DAEMON_LOGS) { }
 };
 
 }
--- src/rus/daemon.cpp
+++ src/rus/daemon.cpp
@@ -19,28 +19,30 @@ int main (int argc, const char* argv[])
 		desc.add_options()
 			("help,h", "print help message")
 			("port,p", po::value<int>()->default_value(daemon::Config::DEFAULT_PORT), "set compression level")
+			("logs,l", po::value<string>()->default_value(DEFAULT_DAEMON_LOGS), "log file")
 		;
 		po::variables_map vm;
-        po::store(po::parse_command_line(argc, argv, desc), vm);
-        po::notify(vm);
+		po::store(po::parse_command_line(argc, argv, desc), vm);
+		po::notify(vm);
 
-        if (vm.count("help")) {
-            cout << desc << "\n";
-            return 0;
-        }
+		if (vm.count("help")) {
+			cout << desc << "\n";
+			return 0;
+		}
 
-        if (vm.count("port")) {
-        	daemon::Config::mod().port = vm["port"].as<int>();
-        	cout << "port: " << daemon::Config::mod().port << endl;
-        }
+		if (vm.count("port")) {
+			daemon::Config::mod().port = vm["port"].as<int>();
+			cout << "port: " << daemon::Config::mod().port << endl;
+		}
+	} catch (const Error& err) {
+		cerr << err.what();
+		return 1;
+	} catch (exception& ex) {
+		cerr << "error: " << ex.what() << "\n";
+		return 1;
+	} catch (...) {
+		cerr << "Exception of unknown type!\n";
+		return 2;
 	}
-    catch (exception& ex) {
-        cerr << "error: " << ex.what() << "\n";
-        return 1;
-    }
-    catch (...) {
-        cerr << "Exception of unknown type!\n";
-        return 2;
-    }
 	return 0;
 }
