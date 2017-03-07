9891ed6daf56b6aff4859ba96061c3c4a324e7ab
Dmitry Vlasov <dvlasov@adjuggler.ru>
2017-03-07 19:37:47 +0700

    Russell PEG parser is added

---
+++ src/rus/parser.cpp
@@ -0,0 +1,287 @@
+#include "rus/sys.hpp"
+#include "peglib.h"
+
+namespace mdl { namespace rus {
+
+struct Parser {
+private:
+	struct Stack {
+		vector<Vars>       vars;
+		vector<Proof*>     proofs;
+		map<Symbol, Type*> typing;
+		void popVars() {
+			Vars& vs = vars.back();
+			for (auto v : vs.v) typing.erase(v.lit);
+			vs.pop_back();
+		}
+		void pushVars(const Vars& var) {
+			for (auto v : var.v) {
+				vars.back().v.push_back(v);
+				typing[v.lit] = v.type;
+			}
+		}
+		void pushVars(const Theorem* thm) {
+			for (auto v : thm->ass.vars.v) {
+				vars.back().v.push_back(v);
+				typing[v.lit] = v.type;
+			}
+		}
+	};
+	typedef Stack Context;
+	peg::parser parser;
+
+public:
+	static const char* run_syntax() {
+		return R"(
+			# Russell grammar
+		
+            SOURCE   <- ELEMENT*
+			ELEMENT  <- COMMENT / IMPORT / CONST / TYPE / RULE / AXIOM / DEF / THEOREM / PROOF
+ 
+			IMPORT   <- 'import' PATH ';;'
+			CONST    <- 'constant' '{' 'symbol' SYMB ';;' ('ascii' SYMB ';;')? ('latex' SYMB ';;')? '}'
+			TYPE     <- 'type' LAB (':' LAB (',' LAB)*) ';;' 
+			RULE     <- 'rule' LAB '(' VARS ')' '{' TERM '}'
+			TERM     <- 'term' ':' LAB '=' '#' EXPR_TR ';;' 
+			DEF      <- 'definition' LAB '(' VARS ')' DISJ '{' HYP* DEF_M DEF_S BAR PROP '}'
+			DEF_M    <- 'defiendum' ':' LAB '=' '#' EXPR ';;' 
+			DEF_S    <- 'definiens' ':' LAB '=' '#' EXPR ';;' 
+			HYP      <- 'hyp' IND ':' LAB '=' '|-' EXPR ';;' 
+			PROP     <- 'prop' IND ':' LAB '=' '|-' EXPR ';;'
+			TERM     <- 'term' ':' LAB '=' '#' EXPR_TR ';;' 
+			AXIOM    <- 'axiom' LAB '(' VARS ')' DISJ '{' (HYP+ BAR)? PROP '}'
+			THEOREM  <- 'theorem' LAB '(' VARS ')' DISJ '{' (HYP+ BAR)? PROP '}'
+			PROOF    <- 'proof' LAB? 'of' LAB '{' PROOF_EL+ '}'
+			PROOF_EL <- 'var' VARS ';;' / STEP / QED
+			DISJ     <- 'disjointed' '(' DISJ_SET (',' DISJ_SET)* ')'
+			DISJ_SET <- LAB+
+
+			STEP     <- 'step' IND ':' LAB '=' STEP_TP LAB ('(' REFS ')')? '|-' EXPR ';;'
+			STEP_TP  <- 'thm' / 'def' / 'axm' / 'claim' / '?'
+			QED      <- 'qed' 'prop' LAB '=' 'step' LAB ';;'
+
+			VARS     <- (VAR (',' VAR)*)?
+			VAR      <- SYMB : LAB
+            BAR      <- '-----' '-'*
+
+			EXPR     <- (SYMB / COMMENT)+
+			SYMB     <- < (![ \t\r\n$] .)+ >
+			LAB      <- < (![,(=;:{ \t\r\n$] .)+ >
+			PATH     <- < (![ \t\r\n$] .)+ >
+			IND      <- < [0-9]+ >
+			COMMENT    <- COMMENT_ML / COMMENT_SL 
+			COMMENT_ML <- '/*' < (!'*/' .)* > '*/'
+			COMMENT_SL <- '//' < (![\n$] .)+ >
+		
+			%whitespace <- [ \t\r\n]*
+		)";
+	}
+	Parser(const Path& path) : parser(mm_syntax()) {
+
+		parser["SYMB"] = [](const peg::SemanticValues& sv) {
+			return Symbol(Lex::toInt(sv.token()));
+		};
+		parser["LAB"] = [](const peg::SemanticValues& sv) {
+			return Lex::toInt(sv.token());
+		};
+		parser["IND"] = [](const peg::SemanticValues& sv) {
+			return (uint)std::stoul(sv.token());
+		};
+		parser["PATH"] = [](const peg::SemanticValues& sv) {
+			return sv.token();
+		};
+		parser["EXPR"] = [](const peg::SemanticValues& sv) {
+			Expr expr;
+			expr.symbols.reserve(sv.size());
+			for (auto& s : sv) {
+				if (s.is<Symbol>()) expr.symbols.push_back(s);
+				else delete s.get<Comment*>();
+			}
+			return expr;
+		};
+		parser["VAR"] = [](const peg::SemanticValues& sv) {
+			Symbol s = sv[0].get<Symbol>();
+			unit t = sv[1].get<uint>();
+			s.type = System::get().math.types[t];
+			return s;
+		};
+		parser["VARS"] = [](const peg::SemanticValues& sv) {
+			Vars vars;
+			vars.v = sv.transform<Symbol>();
+			return vars;
+		};
+		parser["QED"] = [](const peg::SemanticValues& sv) {
+			Vars vars;
+			vars.v = sv.transform<Symbol>();
+			return vars;
+		};
+		parser["CONST"] = [](const peg::SemanticValues& sv, peg::any& context) {
+			Constants* consts = new Constants { sv[0].get<Vect>() };
+			for (Symbol c : consts->expr)
+				context.get<std::shared_ptr<Context>>()->vars.back().consts.insert(c);
+			return consts;
+		};
+		parser["VAR"] = [](const peg::SemanticValues& sv, peg::any& context) {
+			Variables* vars = new Variables { sv[0].get<Vect>() };
+			for (Symbol c : vars->expr)
+				context.get<std::shared_ptr<Context>>()->vars.back().vars.insert(c);
+			return vars;
+		};
+		parser["DISJ"] = [](const peg::SemanticValues& sv, peg::any& context) {
+			Disjointed* disj = new Disjointed { sv[0].get<Vect>() };
+			for (Symbol v : disj->expr)
+				context.get<std::shared_ptr<Context>>()->vars.back().vars.insert(v);
+			return disj;
+		};
+		parser["ESS"] = [](const peg::SemanticValues& sv, peg::any& context) {
+			Essential* ess = new Essential { sv[0].get<uint>(), sv[1].get<Vect>() };
+			markVars(ess->expr, context.get<std::shared_ptr<Context>>()->vars);
+			System::mod().math.essentials[ess->label] = ess;
+			return ess;
+		};
+		parser["FLO"] = [](const peg::SemanticValues& sv, peg::any& context) {
+			Floating* flo = new Floating { sv[0].get<uint>(), sv[1].get<Vect>() };
+			markVars(flo->expr, context.get<std::shared_ptr<Context>>()->vars);
+			System::mod().math.floatings[flo->label] = flo;
+			return flo;
+		};
+		parser["AX"] = [](const peg::SemanticValues& sv, peg::any& context) {
+			Axiom* ax = new Axiom { sv[0].get<uint>(), sv[1].get<Vect>(), (uint) -1 };
+			markVars(ax->expr, context.get<std::shared_ptr<Context>>()->vars);
+			System::mod().math.axioms[ax->label] = ax;
+			return ax;
+		};
+		parser["TH"] = [](const peg::SemanticValues& sv, peg::any& context) {
+			Theorem* th = new Theorem();
+			th->label = sv[0].get<uint>();
+			th->expr  = sv[1].get<Vect>();
+			th->proof = sv[2].get<Proof*>();
+			markVars(th->expr, context.get<std::shared_ptr<Context>>()->vars);
+			System::mod().math.theorems[th->label] = th;
+			return th;
+		};
+		parser["PROOF"] = [](const peg::SemanticValues& sv) {
+			Proof* pr = new Proof();
+			pr->refs = sv.transform<Ref*>();
+			return pr;
+		};
+		parser["REF"] = [](const peg::SemanticValues& sv) {
+			uint lab = sv[0].get<uint>();
+			System::Math& math = System::mod().math;
+			if (math.floatings.count(lab))
+				return new Ref(math.floatings[lab]);
+			else if (math.essentials.count(lab))
+				return new Ref(math.essentials[lab]);
+			else if (math.axioms.count(lab))
+				return new Ref(math.axioms[lab]);
+			else if (math.theorems.count(lab))
+				return new Ref(math.theorems[lab]);
+			else
+				throw Error("unknown label in proof", Lex::toStr(lab));
+		};
+		parser["COMMENT"] = [](const peg::SemanticValues& sv) {
+			string text = sv.token();
+			return new Comment(text.front() == ' ' ? text : " " + text);
+		};
+		parser["ELEMENT"] = [&](const peg::SemanticValues& sv, peg::any& context) {
+			// COMMENT / DISJ / ESS / TH / '${' BLOCK '$}'/ AX / CONST / VAR / FLO / INCLUDE
+			Node node;
+			switch (sv.choice()) {
+			case 0: node = Node(sv[0].get<Comment*>());   break;
+			case 1: node = Node(sv[0].get<Disjointed*>());break;
+			case 2: node = Node(sv[0].get<Essential*>()); break;
+			case 3: node = Node(sv[0].get<Theorem*>());   break;
+			case 4: node = Node(sv[0].get<Block*>());     break;
+			case 5: node = Node(sv[0].get<Axiom*>());     break;
+			case 6: node = Node(sv[0].get<Constants*>()); break;
+			case 7: node = Node(sv[0].get<Variables*>()); break;
+			case 8: node = Node(sv[0].get<Floating*>());  break;
+			case 9: node = Node(sv[0].get<Inclusion*>()); break;
+			}
+			context.get<std::shared_ptr<Context>>()->block->contents.push_back(node);
+			return node;
+		};
+		parser["BLOCK"] = [](const peg::SemanticValues& sv, peg::any& context) {
+			Block* b = context.get<std::shared_ptr<Context>>()->block;
+			b->contents = sv.transform<Node>();
+			init_indexes(b->contents);
+			return b;
+		};
+		parser["BLOCK"].enter = [](peg::any& c) {
+			Context* context = c.get<std::shared_ptr<Context>>().get();
+			context->block = new Block(context->block);
+			if (!context->source) {
+				context->vars.push_back(Scope());
+			} else {
+				context->src_blocks.insert(context->block);
+			}
+			context->source = false;
+		};
+		parser["BLOCK"].leave = [](peg::any& c) {
+			Context* context = c.get<std::shared_ptr<Context>>().get();
+			if (!context->src_blocks.count(context->block)) {
+				context->vars.pop_back();
+			} else {
+				context->src_blocks.erase(context->block);
+			}
+			context->block = context->block->parent;
+		};
+		parser["SOURCE"] = [path](const peg::SemanticValues& sv, peg::any& context) {
+			Source* src = new Source(Lex::toInt(path.name));
+			src->block = sv[0].get<Block*>();
+			return src;
+		};
+		parser["SOURCE"].enter = [](peg::any& context) {
+			context.get<std::shared_ptr<Context>>()->source = true;
+		};
+		parser["INCLUDE"] = [path](const peg::SemanticValues& sv, peg::any& context) {
+			string name = sv.token();
+			static map<string, mm::Inclusion*> included;
+			if (included.count(name)) {
+				mm::Inclusion* inc = included[name];
+				return new mm::Inclusion(inc->source, false);
+			} else {
+				Path new_path(path);
+				new_path.name_ext(name);
+				mm::Inclusion* inc = new mm::Inclusion(nullptr, true);
+				included[name] = inc;
+				inc->source = parse(new_path, context.get<std::shared_ptr<Context>>());
+				return inc;
+			}
+		};
+	}
+
+	static Source* parse(Path path) {
+		path = path.verify();
+		string data;
+		path.read(data);
+		Parser p(path);
+		mdl::mm::Source* src = nullptr;
+		peg::any context(std::make_shared<Context>());
+		if (!p.parser.parse<mdl::mm::Source*>(data.c_str(), context, src)) return nullptr;
+		std::swap(data, src->data);
+		return src;
+	}
+
+private:
+	static void mark_vars(Expr& ex, VarStack& var_stack) {
+		for (auto& s : ex.symbols) {
+			bool is_var = var_stack.typing.count(s.lit);
+			bool is_const = System::get().math.consts.count(s.lit);
+			if (is_const && is_var)
+				throw Error("constant symbol is marked as variable");
+			if (!is_const && !is_var) {
+				string msg = "symbol " + Lex::toStr(s.lit) + " ";
+				msg += " neither constant nor variable";
+				throw Error(msg);
+			}
+			if (is_var) s.type = var_stack.typing[s.lit];
+		}
+	}
+};
+
+Source* parse(const Path& path) {
+	return Parser::parse(path);
+}
+
+}} // mdl::mm
