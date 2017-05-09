67fcfe67c486a6ec601789a868b4505e3ac6a1af
Dmitry Vlasov <vlasov@academ.org>
2017-05-09 11:26:03 +0700

    daemon/console connection implemented

--- include/daem/sys.hpp
+++ include/daem/sys.hpp
@@ -1,11 +1,16 @@
 #pragma once
 
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+
 #include "rus/ast.hpp"
 #include "smm/ast.hpp"
 #include "mm/ast.hpp"
 #include "timer.hpp"
 
 namespace mdl { namespace daemon {
+
+using namespace boost::asio;
 
 #define DEFAULT_DAEMON_LOGS "/var/tmp/mdld.log"
 #define DEFAULT_DAEMON_HOST "localhost"
@@ -24,8 +29,46 @@ struct Daemon {
 
 	static const Daemon& get() { return mod(); }
 	static Daemon& mod() { static Daemon d; return d; }
+
 private:
-	Daemon() : config() { }
+	enum { MAX_MESSAGE_SIZE = 1024 };
+	io_service service;
+	ip::tcp::endpoint endpoint;
+	ip::tcp::acceptor acceptor;
+	ip::tcp::socket   socket;
+	char buffer[MAX_MESSAGE_SIZE];
+	enum State { RUN, EXIT, CLOSE };
+	State state;
+
+	static void session();
+
+	Daemon();
+
+	string get_request();
+	void send_response(const string& response);
 };
+
+struct Console {
+	Config config;
+
+	static const Console& get() { return mod(); }
+	static Console& mod() { static Console c; return c; }
+
+private:
+	enum { MAX_MESSAGE_SIZE = 1024 };
+	io_service service;
+	ip::tcp::resolver resolver;
+	ip::tcp::socket   socket;
+	ip::tcp::endpoint endpoint;
+	int  message_size;
+	char buff[MAX_MESSAGE_SIZE];
+
+	static void session();
+	Console();
+	string get_response();
+	size_t read_complete(const boost::system::error_code& err, size_t bytes) const;
+	void send_request(const string& request);
+};
+
 }} // mdl::daemon
 
--- src/daem/daemon.cpp
+++ src/daem/daemon.cpp
@@ -2,7 +2,7 @@
 
 namespace mdl { namespace daemon {
 
-Lang make_args(const string& c, Args& args) {
+inline Lang make_args(const string& c, Args& args) {
 	std::istringstream command(c);
 	string lang;
 	if (!getline(command, lang, ' ')) return Lang::NONE;
@@ -11,28 +11,88 @@ Lang make_args(const string& c, Args& args) {
 	return chooseLang(lang);
 }
 
-string get_request() {
-	return "";
-}
-void send_response(const string& response) {
-
-}
-
-void Daemon::run() {
-	while (true) {
-		string request = get_request();
-		if (request == "exit" || request == "cancel" || request == "quit") return;
-		Args args;
-		Return ret;
-		Lang lang = make_args(request, args);
-		switch (lang) {
-		case Lang::RUS : ret = rus::Sys::mod().execute(args); break;
-		case Lang::SMM : ret = smm::Sys::mod().execute(args); break;
-		case Lang::MM  : ret = mm::Sys::mod().execute(args);  break;
-		case Lang::NONE: cout << "no language is chosen" << endl; return;
+void Daemon::session() {
+	Daemon& daemon = mod();
+	try {
+		while (true) {
+			string request = daemon.get_request();
+			if (request == "exit" || request == "cancel" || request == "quit") {
+				daemon.state = EXIT;
+				return;
+			}
+			Args args;
+			Return ret;
+			Lang lang = make_args(request, args);
+			switch (lang) {
+			case Lang::RUS : ret = rus::Sys::mod().execute(args); break;
+			case Lang::SMM : ret = smm::Sys::mod().execute(args); break;
+			case Lang::MM  : ret = mm::Sys::mod().execute(args);  break;
+			case Lang::NONE: cout << "no language is chosen" << endl; return;
+			}
+			daemon.send_response(ret.to_string());
 		}
-		send_response(ret.to_string());
+	} catch (std::exception& e) {
+		std::cerr << "Exception in thread: " << e.what() << endl;
 	}
+}
+
+Daemon::Daemon() : config(), endpoint(ip::tcp::v4(), config.port),
+acceptor(service, endpoint), socket(service), state(RUN) {
+	 while (!exit) {
+		acceptor.accept(socket);
+		std::thread(Daemon::session).detach();
+	  }
+}
+
+string Daemon::get_request() {
+	boost::system::error_code error;
+	  size_t length = socket.read_some(boost::asio::buffer(buffer), error);
+	  if (error == boost::asio::error::eof) {
+		state = CLOSE; // Connection closed cleanly by peer.
+		return "";
+	  } else if (error) {
+		throw boost::system::system_error(error); // Some other error.
+	  }
+	return string(buffer, length);
+}
+
+
+void Daemon::send_response(const string& response) {
+	boost::asio::write(socket, boost::asio::buffer(response.c_str(), response.size()));
+}
+
+void Console::session() {
+	Console& console = mod();
+	for (std::string request; std::getline(std::cin, request);) {
+		if (request == "exit" || request == "cancel" || request == "quit") return;
+		console.send_request(request);
+		string response = console.get_response();
+		Return ret = Return::from_string(response);
+		cout << (ret ? "success" : "fail") << ": " << ret.text << endl;
+	}
+}
+
+Console::Console() : config(), resolver(service), socket(service),
+endpoint(*resolver.resolve({config.host, to_string(config.port)})), message_size(-1) {
+	socket.connect(endpoint);
+	std::thread(&Console::session).detach();
+}
+
+size_t Console::read_complete(const boost::system::error_code& err, size_t bytes) const {
+	if (err) return 0;
+	bool found = std::find(buff, buff + bytes, '\n') < buff + bytes;
+	// we read one-by-one until we get to enter, no buffering
+	return found ? 0 : 1;
+}
+
+string Console::get_response() {
+	message_size = 0;
+	read(socket, boost::asio::buffer(buff), boost::bind(&Console::read_complete, this, _1, _2));
+	return std::string(buff, message_size);
+}
+
+void Console::send_request(const string& request) {
+	socket.write_some(boost::asio::buffer(request));
 }
 
 }}
