985cccea9a8f460e397b9bc678846b113c2192b7
Dmitry Vlasov <vlasov@academ.org>
2017-08-03 19:27:34 +0700

    Oracle tactic is added

--- src/rus/prover/rus_prover_space.hpp
+++ src/rus/prover/rus_prover_space.hpp
@@ -13,38 +13,6 @@ struct Tactic {
 	virtual Node* next() = 0;
 };
 
-struct BreadthSearch : public Tactic {
-	void add(Node* n) override {
-		if (Prop* p = dynamic_cast<Prop*>(n))
-			leafs.push_back(p);
-	}
-	void del(Node* n) override {
-		if (n) leafs.erase(std::find(leafs.begin(), leafs.end(), n));
-	}
-	Node* next() override {
-		Prop* n = leafs.front();
-		leafs.erase(leafs.begin());
-		return n;
-	}
-private:
-	vector<Prop*> leafs;
-};
-
-struct MetaTactic : public Tactic {
-	void add(Node* n) override {
-		for (auto t : tactics) t->add(n);
-	}
-	void del(Node* n) override {
-		if (n) for (auto t : tactics) t->del(n);
-	}
-	Node* next() override {
-		return tactics[tactic()]->next();
-	}
-	virtual uint tactic() = 0;
-private:
-	vector<Tactic*> tactics;
-};
-
 struct Space {
 	Tactic*       tactic;
 	Hyp           root;
