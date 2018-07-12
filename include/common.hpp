49f0db16f764f9cfe4eace3234bcc158871498b7
Dmitry Vlasov <vlasov@academ.org>
2018-07-12 12:47:18 +0700

    set_unique_ptr is added

--- include/common.hpp
+++ include/common.hpp
@@ -14,6 +14,25 @@
 
 namespace mdl {
 
+template<class T>
+struct maybe_deleter {
+	explicit maybe_deleter(bool doit = true) : _delete(doit) { }
+
+	void operator() (T* p) const{
+		if (_delete) delete p;
+	}
+private:
+	bool _delete;
+};
+
+template<class T>
+using set_unique_ptr = std::unique_ptr<T, maybe_deleter<T>>;
+
+template<class T>
+set_unique_ptr<T> make_find_ptr(T* raw){
+	return set_unique_ptr<T>(raw, maybe_deleter<T>(false));
+}
+
 template<class T> struct Undef;
 template<> struct Undef<uint> {
 	static uint get()        { return UNDEF_UINT; }
