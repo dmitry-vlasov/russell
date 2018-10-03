1b452b80fa7766a2b625045a5f15eb6f51d7e38c
Dmitry Vlasov <vlasov@academ.org>
2018-10-03 16:12:07 +0700

    trie index map is added

--- src/rus/prover/trie_index/rus_prover_trie_index.hpp
+++ src/rus/prover/trie_index/rus_prover_trie_index.hpp
@@ -25,4 +25,48 @@ struct TrieIndex {
 	unique_ptr<Node> root;
 };
 
+template<class Data>
+struct TrieIndexMap {
+	struct Unified {
+		Unified(const Data& d, Subst&& s) : data(d), sub(std::move(s)) { }
+		Data  data;
+		Subst sub;
+	};
+	void add(const LightTree& t, const Data& d) {
+		index_.add(convert2flatterm(t));
+		data_.push_back(d);
+	}
+	vector<Unified> unify(const LightTree& t) {
+		vector<Unified> ret;
+		TrieIndex::Unified unif = index_.unify(convert2flatterm(t));
+		for (auto& p : unif) {
+			if (p.second.ok) {
+				ret.emplace_back(data_[p.first], std::move(p.second));
+			}
+		}
+		return ret;
+	}
+	string show() const {
+		vector<pair<FlatTerm, uint>> terms = index_.unpack();
+		if (!terms.size()) {
+			return "\n";
+		} else {
+			string ret;
+			for (const auto&  p : terms) {
+				Data d = data_[p.second];
+				ret += "[" + p.first.show() + "]" + " -> " + to_string(d) + "\n";
+			}
+			return ret;
+		}
+	}
+
+	const TrieIndex& index() const { return index_; }
+	const vector<Data>& data() const { return data_; }
+
+private:
+	TrieIndex index_;
+	vector<Data> data_;
+};
+
+
 }}}}
