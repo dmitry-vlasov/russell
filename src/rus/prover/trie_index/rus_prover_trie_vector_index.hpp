17a2774778e4da36a257ddcb5a63a8a279fd284f
Dmitry Vlasov <vlasov@academ.org>
2018-11-21 11:31:53 +0700

    initial implementation of matrix column intersection

--- src/rus/prover/trie_index/rus_prover_trie_vector_index.hpp
+++ src/rus/prover/trie_index/rus_prover_trie_vector_index.hpp
@@ -4,45 +4,77 @@
 
 namespace mdl { namespace rus { namespace prover { namespace trie_index {
 
+struct VectorUnified {
+	struct Cell {
+		Cell(const vector<uint>& ex, bool em) : extra_inds(ex), is_empty(em) {
+			sort(extra_inds.begin(), extra_inds.end());
+		}
+		bool extraContains(uint i) const {
+			return std::binary_search(extra_inds.begin(), extra_inds.end(), i);
+		}
+		vector<uint> extra_inds;
+		bool is_empty;
+	};
+	vector<Cell> vect;
+	GeneralUnified unified;
+	vector<uint> complete(const vector<uint>& v) const {
+		vector<uint> ret(vect.size(), -1);
+		for (uint i = 0, j = 0; i < vect.size(); ++ i) {
+			if (!vect.at(i).is_empty) {
+				ret[i] = v.at(j++);
+			}
+		}
+		return ret;
+	}
+};
+
 struct VectorIndex {
 	VectorIndex(uint dim) : vect(dim) { }
 	struct Cell {
 		void init(const vector<uint>& all_inds) {
-			for (uint i : all_inds) {
-				if (inds_in_exprs.find(i) == inds_in_exprs.end()) {
+			all_inds_ = all_inds;
+			std::sort(inds_in_exprs.begin(), inds_in_exprs.end());
+			for (uint i : all_inds_) {
+				if (!std::binary_search(inds_in_exprs.begin(), inds_in_exprs.end(), i)) {
 					extra_inds_.push_back(i);
 				}
 			}
 		}
 		void add(const LightTree& e, uint i) {
-			inds_in_exprs.insert(i);
+			inds_in_exprs.push_back(i);
 			exprs_.add(e, i);
 		}
 		const TrieIndex& exprs() const { return exprs_; }
 		const vector<uint>& extraInds() const { return extra_inds_; }
+		const vector<uint>& allInds() const { return all_inds_; }
 
 	private:
 		TrieIndex    exprs_;
 		vector<uint> extra_inds_;
-		set<uint>    inds_in_exprs;
+		vector<uint> all_inds_;
+		vector<uint> inds_in_exprs;
 	};
-	void unify_general() {
+	VectorUnified unify_general() {
 		vector<BothIter> iters;
+		VectorUnified ret;
 		for (const auto& c : vect) {
+			ret.vect.emplace_back(c.extraInds(), c.exprs().empty());
 			if (!c.exprs().empty()) {
 				iters.emplace_back(TrieIndex::TrieIter(c.exprs().root));
 			}
 		}
-		unified = trie_index::unify_general(iters);
+		ret.unified = trie_index::unify_general(iters);
 		/*for (auto& p : unif) {
 			if (p.second.sub.ok) {
 				//cout << "UNIFIED: " << p.first << endl;
 				ret.emplace_back(m.data().at(p.first[0]), convert2subst(p.second.sub));
 			}
 		}*/
+		return ret;
 	}
 	vector<Cell> vect;
-	GeneralUnified unified;
 };
+
+VectorUnified intersect(const VectorUnified& vu1, const VectorUnified& vu2);
 
 }}}}
