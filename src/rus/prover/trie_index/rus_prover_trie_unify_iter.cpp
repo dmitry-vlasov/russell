db8680e7d35eb5048d226f9cc11eb0fd80458606
Dmitry Vlasov <vlasov@academ.org>
2019-03-14 12:41:14 +0700

    reverted case of iters size=1 in case of non-tree unify

--- src/rus/prover/trie_index/rus_prover_trie_unify_iter.cpp
+++ src/rus/prover/trie_index/rus_prover_trie_unify_iter.cpp
@@ -188,18 +188,12 @@ vector<UnifyIters> unify_general_2(const UnifyIters& inits) {
 	//}
 
 	vector<UnifyIters> ret;
-	if (inits.iters.size() <= 1) {
-		cout << "FUCKL!!!" << endl;
-	}
-	assert(inits.iters.size() > 1);
-	/*if (inits.iters.size() > 0) {
+	if (inits.iters.size() > 0) {
 		if (inits.iters.size() == 1) {
-			cout << "WOT ONO" << endl;
-			for (const auto& end : inits.iters[0].allEnds()) {
-				cout << "\tend: " << end.show() << endl;
+			for (const auto& end : inits.iters[0].ends()) {
 				ret.emplace_back(vector<MultyIter>(1, end), inits.parentSub, inits.sub);
 			}
-		} else {*/
+		} else {
 
 			struct UnifyPair {
 				UnifyPair(const UnifyIters& b) : is_root(true), beg(b), cur(b) { }
@@ -246,8 +240,8 @@ vector<UnifyIters> unify_general_2(const UnifyIters& inits) {
 					}
 				}
 			}
-	//	}
-	//}
+		}
+	}
 	return ret;
 }
 
