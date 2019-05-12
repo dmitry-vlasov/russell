7b1581a85afb40ee2e53c2c1a97f92a2a3c13cdc
dmitry-vlasov <vlasov@academ.org>
2019-05-12 19:22:00 +0700

    useless stuff is removed

--- src/rus/prover/unify/rus_prover_unify.hpp
+++ src/rus/prover/unify/rus_prover_unify.hpp
@@ -5,7 +5,6 @@
 
 namespace mdl { namespace rus { namespace prover { namespace unify {
 
-string show(const MultyUnifiedSubs&);
 MultyUnifiedSubs unify_subs_matrix(Prop* pr, Hyp* hy, const vector<ProofHypIndexed>& hs, const ProofsSizeLimit*);
 Term unify_general(const vector<Term>& ex, Subst& sub);
 
